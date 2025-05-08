e01184cae9c2e8eff455baeba964621a6e73111b
darthjee <darthjee@gmail.com>
2025-05-08 19:02:14 -0300

    Rubocop fix

--- source/spec/models/oak/photo_spec.rb
+++ source/spec/models/oak/photo_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 RSpec.describe Oak::Photo, type: :model do
--- source/spec/support/factories/oak/photo.rb
+++ source/spec/support/factories/oak/photo.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :oak_photo, class: 'Oak::Photo' do
     association :item, factory: :oak_item
