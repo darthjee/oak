79558a2992d8e9be5196a363f2be8015281b164b
darthjee <darthjee@gmail.com>
2025-05-08 12:08:42 -0300

    Fix module initialization

--- source/app/models/oak/exception.rb
+++ source/app/models/oak/exception.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
-module Oak
-  class Exception < StandardError
-    class LoginFailed  < Oak::Exception; end
-    class Unauthorized < Oak::Exception; end
-    class NotLogged    < Oak::Exception; end
-  end
+class Oak::Exception < StandardError
+  class LoginFailed  < Oak::Exception; end
+  class Unauthorized < Oak::Exception; end
+  class NotLogged    < Oak::Exception; end
 end
