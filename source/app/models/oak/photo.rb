24db1feedef7b0edbb3857594f74f3c3aaa8b7d8
darthjee <darthjee@gmail.com>
2025-05-08 19:01:57 -0300

    Add default ordering

--- source/app/models/oak/photo.rb
+++ source/app/models/oak/photo.rb
@@ -8,11 +8,14 @@ module Oak
     # Validations
     validates :item, presence: true
     validates :order,
-      numericality: {
-        only_integer: true,
-        greater_than_or_equal_to: -127,
-        less_than_or_equal_to: 128
-      },
-      allow_nil: true
+              numericality: {
+                only_integer: true,
+                greater_than_or_equal_to: -127,
+                less_than_or_equal_to: 128
+              },
+              allow_nil: true
+
+    # Default scope to order by 'order'
+    default_scope { order(:order) }
   end
 end
