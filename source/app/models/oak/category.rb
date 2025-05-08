99ac68d60a8cc3cb41015c15d23f28fcab888d96
darthjee <darthjee@gmail.com>
2025-05-08 15:09:02 -0300

    slug comes from name

--- source/app/models/oak/category.rb
+++ source/app/models/oak/category.rb
@@ -2,8 +2,14 @@
 
 module Oak
   class Category < ApplicationRecord
+    # Validates the presence and uniqueness of name and slug
     validates :name, presence: true, uniqueness: true
-
     validates :slug, presence: true, uniqueness: true
+
+    # Overrides the name= method to update the slug
+    def name=(value)
+      super(value)
+      self.slug = value&.to_s&.underscore
+    end
   end
 end
