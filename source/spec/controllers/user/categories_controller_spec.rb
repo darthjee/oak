e98d61ae4166359917193d508d46eaab8e6ab59d
darthjee <darthjee@gmail.com>
2025-05-08 21:08:38 -0300

    Init controller

---
+++ source/spec/controllers/user/categories_controller_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe User::CategoriesController, type: :controller do
+  describe 'GET #index' do
+    let(:categories) { create_list(:oak_category, 3) }
+
+    let(:expected) do
+      Oak::Category::MenuDecorator.new(categories).as_json
+    end
+
+    before do
+      get :index
+    end
+
+    it 'returns a successful response' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders the correct JSON using the decorator' do
+      expect(JSON.parse(response.body)).to eq(expected.map(&:stringify_keys))
+    end
+  end
+end
