a1dc19ff0f5043a87afc858405c49944ad6d61c1
darthjee <darthjee@gmail.com>
2025-05-08 21:08:29 -0300

    Fix controller

--- source/app/controllers/user/categories_controller.rb
+++ source/app/controllers/user/categories_controller.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
-class CategoriesController < ApplicationController
+class User::CategoriesController < ApplicationController
   include OnePageApplication
 
   protect_from_forgery except: %i[index]
 
   resource_for :category,
-               only: :index
-  resource_for :script,
                only: :index,
+               klass: Oak::Category,
                decorator: Oak::Category::MenuDecorator,
                paginated: false
 end
