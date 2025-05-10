46250d7209f5bf09104303f8f9f287ec024101a4
darthjee <darthjee@gmail.com>
2025-05-10 10:27:53 -0300

    Add seed

--- source/db/seeds.rb
+++ source/db/seeds.rb
@@ -1,1 +1,19 @@
 # frozen_string_literal: true
+
+return if Rails.env.production? && ENV['FORCE_SEED'].nil?
+
+Zyra
+  .register(User, find_by: :email)
+  .on(:build) do |user|
+    return if user.encrypted_password.present?
+    user.password = SecureRandom.hex(10)
+  end
+
+Zyra.register(Oak::Category, find_by: :slug)
+
+user = Zyra.find_or_create(
+  :user,
+  email: 'email@srv.com',
+  login: 'user',
+  name: 'user',
+) { |u| u.password = '123456' }
