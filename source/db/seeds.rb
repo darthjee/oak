9ac5af9d46ac2702502968cd0c52178feac50a4b
darthjee <darthjee@gmail.com>
2025-05-10 11:58:21 -0300

    Rubocop fix

--- source/db/seeds.rb
+++ source/db/seeds.rb
@@ -6,14 +6,15 @@ Zyra
   .register(User, find_by: :email)
   .on(:build) do |user|
     return if user.encrypted_password.present?
+
     user.password = SecureRandom.hex(10)
   end
 
 Zyra.register(Oak::Category, find_by: :slug)
 
-user = Zyra.find_or_create(
+Zyra.find_or_create(
   :user,
   email: 'email@srv.com',
   login: 'user',
-  name: 'user',
+  name: 'user'
 ) { |u| u.password = '123456' }
