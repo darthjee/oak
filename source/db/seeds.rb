381c73e932e26ae42b3e8b558ad3ac676cfcddf1
darthjee <darthjee@gmail.com>
2025-05-11 18:24:50 -0300

    Add mock data

--- source/db/seeds.rb
+++ source/db/seeds.rb
@@ -7,14 +7,27 @@ Zyra
   end
 
 Zyra.register(Oak::Category, find_by: :name)
+Zyra.register(Oak::Kind, find_by: :name)
+Zyra.register(Oak::Item, find_by: %i[user_id category_id kind_id name])
 
-Zyra.find_or_create(
+user = Zyra.find_or_create(
   :user,
   email: 'email@srv.com',
   login: 'user',
   name: 'user'
 ) { |u| u.password = '123456' }
 
-%w[Arduino Pokemon Games Miniatures Packages Photos].each do |name|
+category = Zyra.find_or_create(:oak_category, name: 'Arduino')
+kind = Zyra.find_or_create(:oak_kind, name: 'Arduino')
+
+%w[Pokemon Games Miniatures Packages Photos].each do |name|
   Zyra.find_or_create(:oak_category, name:)
 end
+
+Zyra.find_or_create(
+  :oak_item,
+  name: 'Arduino',
+  user_id: user.id,
+  category_id: category.id,
+  kind_id: kind.id
+)
