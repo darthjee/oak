fa8fca3cf323fb9cfb6980e75e453efaee2e9f96
darthjee <darthjee@gmail.com>
2025-05-11 14:26:01 -0300

    Add html spec

--- source/spec/controllers/categories_controller_spec.rb
+++ source/spec/controllers/categories_controller_spec.rb
@@ -6,22 +6,38 @@ RSpec.describe CategoriesController, type: :controller do
   describe 'GET #index' do
     let!(:categories) { create_list(:oak_category, 3) }
 
-    let(:expected) do
-      Oak::Category::MenuDecorator.new(categories).as_json
+    context 'when format is JSON' do
+      let(:expected) do
+        Oak::Category::MenuDecorator.new(categories).as_json
+      end
+
+      let(:parameters) { { ajax: true, format: :json } }
+
+      before do
+        get :index, params: parameters
+      end
+
+      it 'returns a successful response' do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'renders the correct JSON using the decorator' do
+        expect(JSON.parse(response.body)).to eq(expected.map(&:stringify_keys))
+      end
     end
 
-    let(:parameters) { { ajax: true, format: :json } }
+    context 'when format is HTML' do
+      before do
+        get :index, params: { format: :html, ajax: true }
+      end
 
-    before do
-      get :index, params: parameters
-    end
+      it 'returns a successful response' do
+        expect(response).to have_http_status(:ok)
+      end
 
-    it 'returns a successful response' do
-      expect(response).to have_http_status(:ok)
-    end
-
-    it 'renders the correct JSON using the decorator' do
-      expect(JSON.parse(response.body)).to eq(expected.map(&:stringify_keys))
+      it 'renders the correct template' do
+        expect(response).to render_template(:index)
+      end
     end
   end
 end
