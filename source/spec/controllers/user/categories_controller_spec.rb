963876207f8d5515cdc35445ba98976322a15f9a
darthjee <darthjee@gmail.com>
2025-05-11 21:31:29 -0300

    Hadle logged user

--- source/spec/controllers/user/categories_controller_spec.rb
+++ source/spec/controllers/user/categories_controller_spec.rb
@@ -30,11 +30,42 @@ RSpec.describe User::CategoriesController, type: :controller do
       let(:session) { create(:session, user: create(:user)) }
       let(:user) { session.user }
 
+      before do
+        controller.send(:cookies).signed[:session] = session.id
+      end
+
       context "when there is no subscription" do
         let(:expected) { [] }
 
         before do
-          controller.send(:cookies).signed[:session] = session.id
+          get :index, params: parameters
+        end
+
+        after do
+          controller.send(:cookies).signed[:session] = nil
+        end
+
+        it 'returns a successful response' do
+          expect(response).to have_http_status(:ok)
+        end
+
+        it 'renders the correct JSON using the decorator' do
+          expect(JSON.parse(response.body)).to eq(expected.map(&:stringify_keys))
+        end
+      end
+
+      context "when there are subscriptions" do
+        let!(:subscribed_categories) { create_list(:oak_category, 2) }
+
+        let(:expected) do
+          Oak::Category::MenuDecorator.new(subscribed_categories).as_json
+        end
+
+        before do
+          subscribed_categories.each do |category|
+            create(:oak_subscription, user:, category:)
+          end
+
           get :index, params: parameters
         end
 
