4b883027cb22ebe5dff2712ce498412855bad50c
darthjee <darthjee@gmail.com>
2025-05-13 06:11:10 -0300

    Add spec

--- source/spec/controllers/categories_controller_spec.rb
+++ source/spec/controllers/categories_controller_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 
 RSpec.describe CategoriesController, type: :controller do
   let(:response_json) { JSON.parse(response.body) }
-  
+
   describe 'GET #index' do
     context 'when format is JSON' do
       let!(:categories) { create_list(:oak_category, 3) }
@@ -90,6 +90,57 @@ RSpec.describe CategoriesController, type: :controller do
       end
 
       it 'renders the correct JSON using the decorator' do
+        expect(response_json).to eq(expected.stringify_keys)
+      end
+    end
+  end
+  
+  describe 'POST #create' do
+    let(:category_params) { { name: 'New Category' } }
+    let(:parameters) { { category: category_params, format: :json } }
+    let(:created_category) { Oak::Category.last }
+    let(:expected) { Oak::Category::IndexDecorator.new(created_category).as_json }
+
+    context 'when the request is valid' do
+      it 'creates a new Oak::Category' do
+        expect { post :create, params: parameters }
+          .to change(Oak::Category, :count).by(1)
+      end
+
+      it do
+        post :create, params: parameters
+
+        expect(response).to have_http_status(:created)
+      end
+
+      it 'returns the created category as JSON' do
+        post :create, params: parameters
+
+        expect(response_json).to eq(expected.stringify_keys)
+      end
+    end
+
+    context 'when the request is invalid' do
+      let(:category_params) { { name: '' } }
+      let(:expected_category) { Oak::Category.new(category_params) }
+      let(:expected) do
+        Oak::Category::IndexDecorator.new(expected_category).as_json
+      end
+
+      it 'does not create a new Oak::Category' do
+        expect { post :create, params: parameters }
+          .not_to change(Oak::Category, :count)
+      end
+
+      it do
+        post :create, params: parameters
+
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it 'returns errors as JSON' do
+        post :create, params: parameters
+
         expect(response_json).to eq(expected.stringify_keys)
       end
     end
