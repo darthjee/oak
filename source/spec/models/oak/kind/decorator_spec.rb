0e20a983ff30303fcd9a3d25c344923310575fc4
darthjee <darthjee@gmail.com>
2025-05-17 10:27:07 -0300

    Init error spec

--- source/spec/models/oak/kind/decorator_spec.rb
+++ source/spec/models/oak/kind/decorator_spec.rb
@@ -7,58 +7,32 @@ RSpec.describe Oak::Kind::Decorator do
 
   let(:kind) { build(:oak_kind, name:) }
   let(:slug) { kind.slug }
-  let(:name) { 'Sample Category' }
+  let(:name) { 'Sample Kind' }
 
   describe '#as_json' do
     let(:expected) do
       {
         name:,
-        slug:,
-        snap_url:
+        slug:
       }.stringify_keys
     end
 
-    context 'when there is no item' do
-      let(:snap_url) do
-        [Settings.photos_server_url, 'kind.png'].join('/')
-      end
-
+    context 'when kind is valid' do
       it 'includes the name' do
         expect(decorator.as_json).to eq(expected)
       end
     end
 
-    context 'when there are items without photos' do
-      let!(:item) { create(:oak_item, kind:) }
-      let(:user) { item.user }
-
-      let(:snap_url) do
-        [Settings.photos_server_url, 'kind.png'].join('/')
+    context 'when kind is invalid' do
+      let(:name) { nil }
+      let(:expected) do
+        {
+          name:,
+          slug:
+        }.stringify_keys
       end
-
-      it 'includes the name' do
-        expect(decorator.as_json).to eq(expected)
-      end
-    end
-
-    context 'when there are items with photo' do
-      let!(:item) { create(:oak_item, kind:) }
-      let!(:photo) { create(:oak_photo, item:) }
-      let(:user) { item.user }
-
-      let(:snap_url) do
-        [
-          Settings.photos_server_url,
-          :snaps,
-          :users,
-          user.id,
-          :items,
-          item.id,
-          photo.file_name
-        ].join('/')
-      end
-
-      it 'includes the name' do
+      
+      it 'includes the errors' do
         expect(decorator.as_json).to eq(expected)
       end
     end
