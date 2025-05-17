a72ed70d4a9d418f039840f212880d6915c0b898
darthjee <darthjee@gmail.com>
2025-05-17 13:41:38 -0300

    Fix js controller

--- source/app/assets/javascripts/controllers/item/controller.js
+++ source/app/assets/javascripts/controllers/item/controller.js
@@ -4,18 +4,39 @@
   ]);
 
   var Methods = {
+    requestKinds: function() {
+      var promise = this._getKindsRequester().request();
+      promise.then(this._setData);
+
+      this.constructor.trigger(this, this.route, "request");
+    },
+    _getKindsRequester: function() {
+      if ( !this.kindsRequester ) {
+        this._buildKindsRequester();
+      }
+
+      return this.kindsRequester;
+    },
+    _buildKindsRequester: function() {
+      this.kindsRequester = this.requesterBuilder.build({
+        search: this.location.$$search,
+        path: "/kinds"
+      });
+      this.kindsRequester.bind(this);
+    }
   }
 
   var options = {
-    routeParams: {},
     callback: function(){
       _.extend(this, Methods);
+      this.requestKinds();
     }
   };
 
   app.controller("Item.Controller", [
     "cyberhawk_builder", function(builder) {
       builder.buildAndRequest(this, options);
+
     }
   ]);
 }(window._, window.angular, window.Cyberhawk));
