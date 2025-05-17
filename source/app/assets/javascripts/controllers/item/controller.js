90fe06646af644769fcc2c6a5001b97a340ff1b9
darthjee <darthjee@gmail.com>
2025-05-17 13:44:11 -0300

    Add setKinds

--- source/app/assets/javascripts/controllers/item/controller.js
+++ source/app/assets/javascripts/controllers/item/controller.js
@@ -6,10 +6,17 @@
   var Methods = {
     requestKinds: function() {
       var promise = this._getKindsRequester().request();
-      promise.then(this._setData);
+      promise.then(this._setKinds);
 
       this.constructor.trigger(this, this.route, "request");
     },
+
+    _setKinds: function(response) {
+      this.kinds = response.data;
+      this.loaded = true;
+      this.constructor.trigger(this, this.route, "loaded");
+    },
+
     _getKindsRequester: function() {
       if ( !this.kindsRequester ) {
         this._buildKindsRequester();
