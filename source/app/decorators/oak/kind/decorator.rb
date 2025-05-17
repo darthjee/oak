3668e129f741c81a5c30f41d01cfcf80de3e9486
darthjee <darthjee@gmail.com>
2025-05-17 11:51:08 -0300

    Add kind snap url

--- source/app/decorators/oak/kind/decorator.rb
+++ source/app/decorators/oak/kind/decorator.rb
@@ -5,6 +5,19 @@ module Oak
     class Decorator < ModelDecorator
       expose :name
       expose :slug
+      expose :snap_url
+
+      def snap_url
+        return [base_url, 'kind.png'].join('/') if main_photo.nil?
+
+        Photo::FileUrl.call(main_photo, :snap)
+      end
+
+      private
+
+      def base_url
+        Settings.photos_server_url
+      end
     end
   end
 end
