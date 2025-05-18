611acaf22d62b5e3ff9bdd49ff3333e321a7fe2b
darthjee <darthjee@gmail.com>
2025-05-18 17:34:25 -0300

    Require user for new route

--- source/app/controllers/concerns/user_required.rb
+++ source/app/controllers/concerns/user_required.rb
@@ -9,9 +9,18 @@ module UserRequired
 
     redirection_rule :render_forbidden, :missing_user?
     skip_redirection_rule :render_root, :missing_user?
+
+    delegate :require_user_actions, to: :class
   end
 
   class_methods do
+    def require_user_for(*actions)
+      require_user_actions.merge(actions.map(&:to_sym))
+    end
+
+    def require_user_actions
+      @require_user_actions ||= Set.new
+    end
   end
 
   def render_forbidden
@@ -20,7 +29,7 @@ module UserRequired
 
   def missing_user?
     return false if logged_user
-    return false unless action_name.to_sym == :new
+    return false unless require_user_actions.include?(action_name.to_sym)
     true
   end
 end
