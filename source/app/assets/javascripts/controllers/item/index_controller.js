c8118e9f5dc40f0dd2bd286a1bd5d4c8b2fa680e
darthjee <darthjee@gmail.com>
2025-05-18 18:32:46 -0300

    Remove uneeded method

--- source/app/assets/javascripts/controllers/item/index_controller.js
+++ source/app/assets/javascripts/controllers/item/index_controller.js
@@ -3,45 +3,6 @@
     "cyberhawk/builder"
   ]);
 
-  var LoginMethods = {
-    login: function() {
-      this.logged = true;
-    },
-    logoff: function() {
-      this.logged = false;
-    }
-  }
-
-  var KindMethods = {
-    requestKinds: function() {
-      var promise = this._getKindsRequester().request();
-      promise.then(this._setKinds);
-
-      this.constructor.trigger(this, this.route, "request");
-    },
-
-    _setKinds: function(response) {
-      this.kinds = response.data;
-      this.loaded = true;
-      this.constructor.trigger(this, this.route, "loaded");
-    },
-
-    _getKindsRequester: function() {
-      if ( !this.kindsRequester ) {
-        this._buildKindsRequester();
-      }
-
-      return this.kindsRequester;
-    },
-    _buildKindsRequester: function() {
-      this.kindsRequester = this.requesterBuilder.build({
-        search: this.location.$$search,
-        path: "/kinds"
-      });
-      this.kindsRequester.bind(this);
-    }
-  };
-
   var CategoryMethods = {
     requestCategory: function() {
       var promise = this._getCategoryRequester().request();
@@ -76,18 +37,9 @@
 
   var options = {
     callback: function(){
-      _.extend(this, KindMethods);
       _.extend(this, CategoryMethods);
-      _.extend(this, LoginMethods);
-      _.bindAll(this, "requestKinds", "_setKinds", "_getKindsRequester", "_buildKindsRequester");
       _.bindAll(this, "requestCategory", "_setCategory", "_getCategoryRequester", "_buildCategoryRequester");
-      _.bindAll(this, "login", "logoff");
 
-      this.notifier.register("login-success", this.login);
-      this.notifier.register("logged", this.login);
-      this.notifier.register("logoff-success", this.logoff);
-
-      this.requestKinds();
       this.requestCategory();
     }
   };
