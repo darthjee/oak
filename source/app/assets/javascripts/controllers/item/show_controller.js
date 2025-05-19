fad400430b65046c39be0f6fa4d04f503adbf357
darthjee <darthjee@gmail.com>
2025-05-19 16:17:49 -0300

    Remove uneeded methods

--- source/app/assets/javascripts/controllers/item/show_controller.js
+++ source/app/assets/javascripts/controllers/item/show_controller.js
@@ -3,45 +3,7 @@
     "cyberhawk/builder"
   ]);
 
-  var PhotoMethods = {
-    requestPhotos: function() {
-      var promise = this._getPhotosRequester().request();
-      promise.then(this._setPhotos);
-
-      this.constructor.trigger(this, this.route, "request");
-    },
-
-    _setPhotos: function(response) {
-      this.photos = response.data;
-      this.loaded = true;
-      this.constructor.trigger(this, this.route, "loaded");
-    },
-
-    _getPhotosRequester: function() {
-      if (!this.photosRequester) {
-        this._buildPhotosRequester();
-      }
-
-      return this.photosRequester;
-    },
-
-    _buildPhotosRequester: function() {
-      this.photosRequester = this.requesterBuilder.build({
-        search: this.location.$$search,
-        path: "/photos"
-      });
-      this.photosRequester.bind(this);
-    }
-  };
-
-  var options = {
-    callback: function() {
-      _.extend(this, PhotoMethods);
-      _.bindAll(this, "requestPhotos", "_setPhotos", "_getPhotosRequester", "_buildPhotosRequester");
-
-      this.requestPhotos();
-    }
-  };
+  var options = {};
 
   app.controller("Item.ShowController", [
     "cyberhawk_builder", function(builder) {
