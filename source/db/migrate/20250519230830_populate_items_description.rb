e0323bc0dab5c77190ba38553fe9b2eff61f1ead
darthjee <darthjee@gmail.com>
2025-05-19 20:10:32 -0300

    Populate description

---
+++ source/db/migrate/20250519230830_populate_items_description.rb
@@ -0,0 +1,15 @@
+class PopulateItemsDescription < ActiveRecord::Migration[7.2]
+  def up
+    execute <<~SQL
+      UPDATE items
+      SET description = name
+    SQL
+  end
+
+  def down
+    execute <<~SQL
+      UPDATE items
+      SET description = NULL
+    SQL
+  end
+end
