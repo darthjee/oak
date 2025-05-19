b5f6b01cb72a50f919455bc3b25dabf26f74ce4b
darthjee <darthjee@gmail.com>
2025-05-19 15:39:01 -0300

    Add ItemShowDecorator

---
+++ source/app/decorators/oak/item/show_decorator.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Oak
+  class Item
+    class ShowDecorator < ModelDecorator
+      expose :id
+      expose :name
+      expose :category_slug
+      expose :kind_slug
+      expose :photos
+    end
+  end
+end
