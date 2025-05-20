904fc7541c1700d9a25a946cd5eef42df2ba3a1c
darthjee <darthjee@gmail.com>
2025-05-20 20:18:33 -0300

    Add builder

---
+++ source/app/builders/builder.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Oak
+  class Item
+    class Builder
+      def self.build(**params)
+        new(**params).build
+      end
+
+      def initialize(**params)
+        @params = params.slice(:name, :description, :category, :kind, :user)
+      end
+
+      def build
+        Oak::Item.new(params)
+      end
+
+      private
+
+      attr_reader :params
+    end
+  end
+end
