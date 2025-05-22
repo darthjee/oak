da3b8a92362a8724ac57638594800926437f44c7
Fernando Favini <darthjee@gmail.com>
2025-05-21 21:01:51 -0300

    New links UI (#30)

--- source/app/assets/javascripts/controllers/item/form_controller.js
+++ source/app/assets/javascripts/controllers/item/form_controller.js
@@ -33,12 +33,40 @@
     }
   };
 
+  var LinksMethods = {
+    bindLinksTriggers: function() {
+      this.constructor.on(this.route, "loaded", this.initLinks);
+    },
+
+    initLinks: function() {
+      if (this.data) {
+        if (!this.data.links) {
+          this.data.links = [{}];
+        }
+        if (!this.data.links[0]) {
+          this.data.links = [{}];
+        }
+      }
+    },
+
+    addLink: function() {
+      this.data.links.push({});
+    },
+
+    removeLink: function(index) {
+      this.data.links.splice(index, 1);
+    },
+  };
+
   var options = {
     callback: function(){
       _.extend(this, KindMethods);
+      _.extend(this, LinksMethods);
       _.bindAll(this, "requestKinds", "_setKinds", "_getKindsRequester", "_buildKindsRequester");
+      _.bindAll(this, "bindLinksTriggers", "initLinks", "addLink", "removeLink");
 
       this.requestKinds();
+      this.bindLinksTriggers();
     }
   };
 
