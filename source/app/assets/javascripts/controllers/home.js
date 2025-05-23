933281109030b9f0ac62f5c6e73c8898bbc74217
darthjee <darthjee@gmail.com>
2025-05-23 19:12:06 -0300

    Use custom home controller

--- source/app/assets/javascripts/controllers/home.js
+++ source/app/assets/javascripts/controllers/home.js
@@ -3,7 +3,14 @@
     "cyberhawk/builder"
   ]);
 
+  var options = {
+    callback: function(){
+    }
+  };
+
   app.controller("Home.Controller", [
-    "cyberhawk_builder", function(builder) { builder.build(this); }
+    "cyberhawk_builder", function(builder) {
+      builder.build(this, options);
+    }
   ]);
 }(window._, window.angular));
