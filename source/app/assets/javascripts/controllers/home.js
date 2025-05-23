33a430f98e39ab4f5a5b20e20ebace8ddf6728dc
darthjee <darthjee@gmail.com>
2025-05-23 19:29:01 -0300

    Request categories

--- source/app/assets/javascripts/controllers/home.js
+++ source/app/assets/javascripts/controllers/home.js
@@ -4,13 +4,14 @@
   ]);
 
   var options = {
+    path: '/categories',
     callback: function(){
     }
   };
 
   app.controller("Home.Controller", [
     "cyberhawk_builder", function(builder) {
-      builder.build(this, options);
+      builder.buildAndRequest(this, options);
     }
   ]);
 }(window._, window.angular));
