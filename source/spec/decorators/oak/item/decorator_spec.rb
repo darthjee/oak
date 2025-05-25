9a39acbc53765e9859bb3361ac4ea188b7c1765d
darthjee <darthjee@gmail.com>
2025-05-24 22:59:43 -0300

    Revert "Remove links in favor of main link"
    This reverts commit 9a0e7021215af938303569dd50de020e800e7f3e.

--- source/spec/decorators/oak/item/decorator_spec.rb
+++ source/spec/decorators/oak/item/decorator_spec.rb
@@ -14,7 +14,6 @@ RSpec.describe Oak::Item::Decorator do
   let(:snap_url) do
     [Settings.photos_server_url, 'category.png'].join('/')
   end
-  let(:main_link) { nil }
 
   describe '#as_json' do
     let(:expected) do
@@ -25,7 +24,7 @@ RSpec.describe Oak::Item::Decorator do
         category_slug:,
         kind_slug:,
         snap_url:,
-        link: main_link
+        links: []
       }.stringify_keys
     end
 
@@ -54,28 +53,6 @@ RSpec.describe Oak::Item::Decorator do
       end
     end
 
-    context 'when the item has links' do
-      let!(:link1) { create(:oak_link, item:, order: 2, url: 'https://example.com/2') }
-      let!(:link2) { create(:oak_link, item:, order: 1, url: 'https://example.com/1') }
-      let(:main_link) { Oak::Link::Decorator.new(link2).as_json }
-
-      let(:expected) do
-        {
-          id: item.id,
-          name:,
-          description:,
-          category_slug:,
-          kind_slug:,
-          snap_url:,
-          link: main_link
-        }.deep_stringify_keys
-      end
-
-      it 'includes the main link' do
-        expect(decorator.as_json).to eq(expected)
-      end
-    end
-
     context 'when the item is invalid' do
       let(:name) { '' }
       let(:errors) { { name: ["can't be blank"] } }
@@ -88,7 +65,7 @@ RSpec.describe Oak::Item::Decorator do
           kind_slug:,
           snap_url:,
           errors:,
-          link: main_link
+          links: []
         }.deep_stringify_keys
       end
 
