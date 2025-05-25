5cebe15ba3831180a732860582222d2c4f3026d4
darthjee <darthjee@gmail.com>
2025-05-24 23:11:10 -0300

    Add missing files

---
+++ source/app/decorators/oak/item/form_decorator.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module Oak
+  class Item
+    class FormDecorator < ModelDecorator
+      expose :id
+      expose :name
+      expose :description
+      expose :category_slug
+      expose :kind_slug
+      expose :snap_url
+      expose :links, decorator: Oak::Link::Decorator
+
+      def snap_url
+        return [base_url, 'category.png'].join('/') if main_photo.nil?
+
+        Photo::FileUrl.call(main_photo, :snap)
+      end
+
+      def category_slug
+        object.category&.slug
+      end
+
+      def kind_slug
+        object.kind&.slug
+      end
+
+      private
+
+      def base_url
+        Settings.photos_server_url
+      end
+    end
+  end
+end
