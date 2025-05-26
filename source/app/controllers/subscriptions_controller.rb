777854f500942be499d7afac9f3d1d2740f4917d
darthjee <darthjee@gmail.com>
2025-05-26 18:38:18 -0300

    Refactor controller

--- source/app/controllers/subscriptions_controller.rb
+++ source/app/controllers/subscriptions_controller.rb
@@ -7,18 +7,27 @@ class SubscriptionsController < ApplicationController
   require_user_for :create
 
   def create
-    render json: subscription_json, status: status
+    render json: serialized_subscription, status: subscription_status
   end
 
   private
 
-  def subscription_json
+  def serialized_subscription
     Oak::Subscription::Decorator.new(subscription).as_json
   end
 
   def subscription
-    logged_user.subscriptions.find_or_create_by(category:)
+    @subscription ||= find_or_initialize_subscription
   end
+
+  def find_or_initialize_subscription
+    logged_user.subscriptions.find_or_initialize_by(category: category).tap do |entity|
+      @subscription_status = entity.persisted? ? :ok : :created
+      entity.save if entity.new_record?
+    end
+  end
+
+  attr_reader :subscription_status
 
   def category
     @category ||= Oak::Category.find_by!(slug: params.require(:category_slug))
