a07ba2a81eecd726f1da38a913ee3d0b0a416740
darthjee <darthjee@gmail.com>
2025-05-26 19:21:23 -0300

    Add kinds to categories

--- source/app/models/oak/category.rb
+++ source/app/models/oak/category.rb
@@ -10,5 +10,9 @@ module Oak
     has_many :items, class_name: 'Oak::Item', foreign_key: :category_id, dependent: :destroy
     has_one :sample_item, -> { order(:id) }, class_name: 'Oak::Item'
     has_one :main_photo, -> { order(:id) }, class_name: 'Oak::Photo', through: :sample_item
+
+    # Association with kinds through category_kinds
+    has_many :category_kinds, class_name: 'Oak::CategoryKind', dependent: :destroy
+    has_many :kinds, through: :category_kinds, source: :kind
   end
 end
