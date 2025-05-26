d00044e2be9a9710eca811071dbb364819765afb
darthjee <darthjee@gmail.com>
2025-05-26 17:19:54 -0300

    Add controller

---
+++ source/app/controllers/subscriptions_controller.rb
@@ -0,0 +1,23 @@
+class SubscriptionsController < ApplicationController
+  include UserRequired
+
+  protect_from_forgery except: :create
+  require_user_for :create
+
+  resource_for Oak::Subscription,
+               only: :create,
+               decorator: Oak::Subscription::Decorator
+
+  private
+
+  def subscription_params
+    {
+      user: logged_user,
+      category: category
+    }
+  end
+
+  def category
+    @category ||= Oak::Category.find_by!(slug: params.require(:category_slug))
+  end
+end
