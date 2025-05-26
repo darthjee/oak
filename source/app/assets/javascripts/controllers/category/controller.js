d670d480fe2195a813d253e465ecf2226a10846f
darthjee <darthjee@gmail.com>
2025-05-26 20:51:58 -0300

    Add kind methods

--- source/app/assets/javascripts/controllers/category/controller.js
+++ source/app/assets/javascripts/controllers/category/controller.js
@@ -3,6 +3,36 @@
     "cyberhawk/builder"
   ]);
 
+  var KindMethods = {
+    requestKinds: function() {
+      var promise = this._getKindsRequester().request();
+      promise.then(this._setKinds);
+
+      this.constructor.trigger(this, this.route, "request");
+    },
+
+    _setKinds: function(response) {
+      this.kinds = response.data;
+      this.loaded = true;
+      this.constructor.trigger(this, this.route, "loaded");
+    },
+
+    _getKindsRequester: function() {
+      if ( !this.kindsRequester ) {
+        this._buildKindsRequester();
+      }
+
+      return this.kindsRequester;
+    },
+    _buildKindsRequester: function() {
+      this.kindsRequester = this.requesterBuilder.build({
+        search: this.location.$$search,
+        path: "/kinds"
+      });
+      this.kindsRequester.bind(this);
+    }
+  };
+
   var SubscriptionsMethods = {
     subscribe: function(categorySlug) {
       var path = "/categories/" + categorySlug + "/subscriptions";
@@ -24,6 +54,10 @@
   var options = {
     callback: function() {
       _.extend(this, SubscriptionsMethods);
+      _.extend(this, KindMethods);
+      _.bindAll(this, "requestKinds", "_setKinds", "_getKindsRequester", "_buildKindsRequester");
+
+      this.requestKinds();
 
       _.bindAll(this, "subscribe");
     }
