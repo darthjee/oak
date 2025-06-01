3fc23dffdb07d1941d6be43f259b0d5465399973
darthjee <darthjee@gmail.com>
2025-06-01 17:23:15 -0300

    Add controller

---
+++ source/app/controllers/category/kinds_controller.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class Category < ApplicationRecord
+  class KindsController < ApplicationController
+    include UserRequired
+
+    protect_from_forgery except: :index
+
+    resource_for Oak::Kind,
+                 only: :index,
+                 decorator: Oak::Kind::Decorator,
+                 paginated: true
+
+    model_for Oak::Category,
+              id_key: :slug,
+              param_key: :category_slug
+
+    private
+
+    def kinds
+      @kinds ||= category.kinds
+    end
+  end
+end
