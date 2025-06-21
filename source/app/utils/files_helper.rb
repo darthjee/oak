74e9cc6010cd4d2fd3ba32b55a1147c591c4710c
darthjee <darthjee@gmail.com>
2025-06-21 20:04:50 -0300

    Extract common code

--- source/app/utils/files_helper.rb
+++ source/app/utils/files_helper.rb
@@ -4,24 +4,34 @@ module FilesHelper
   extend self
 
   def directories_in(path)
-    return [] unless Dir.exist?(path)
-
-    Dir.entries(path).select do |entry|
-      full_path = File.join(path, entry)
-      File.directory?(full_path) && !['.', '..'].include?(entry)
-    end
+    entries_in(path, :directory, ignored_entries: ['.', '..'])
   end
 
   def files_in(path, extension: nil)
+    entries_in(path, :file, extension:)
+  end
+
+  private
+
+  def entries_in(path, type, extension: nil, ignored_entries: nil)
     return [] unless Dir.exist?(path)
 
     Dir.entries(path).select do |entry|
       full_path = File.join(path, entry)
-      File.file?(full_path) && matches_extension?(entry, extension)
+      entry_matches?(full_path, entry, type, extension:, ignored_entries:)
     end
   end
 
-  private
+  def entry_matches?(full_path, entry, type, extension: nil, ignored_entries: nil)
+    return unless File.public_send("#{type}?", full_path)
+    return if ignored_entry?(entry, ignored_entries)
+    matches_extension?(entry, extension)
+  end
+
+  def ignored_entry?(entry, ignored_entries = nil)
+    return false unless ignored_entries
+    ignored_entries.include?(entry)
+  end
 
   def matches_extension?(file_name, extensions)
     return true if extensions.nil?
