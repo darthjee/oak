34f03e633a8cf833d2343aca61b3c653c180b540
Fernando Favini <darthjee@gmail.com>
2026-02-19 22:29:48 -0300

    add ajax test

--- source/spec/controllers/concerns/one_page_application_spec.rb
+++ source/spec/controllers/concerns/one_page_application_spec.rb
@@ -28,6 +28,17 @@ RSpec.describe OnePageApplication, type: :controller do
         it 'redirects to hash path' do
           expect(response).to redirect_to('#/index.html')
         end
+
+        context "with ajax param" do
+          before do
+            get :index, params: { format: :html, ajax: true }
+          end
+
+          it 'does not redirect' do
+            expect(response).to have_http_status(:ok)
+            expect(response.body).to eq('ok')
+          end
+        end
       end
 
       context "with json format" do
@@ -59,6 +70,17 @@ RSpec.describe OnePageApplication, type: :controller do
         it 'redirects to external domain with hash path' do
           expect(response).to redirect_to('https://example.com/#/index.html')
         end
+
+        context "with ajax param" do
+          before do
+            get :index, params: { format: :html, ajax: true }
+          end
+
+          it 'does not redirect' do
+            expect(response).to have_http_status(:ok)
+            expect(response.body).to eq('ok')
+          end
+        end
       end
 
       context "with json format" do
@@ -89,6 +111,17 @@ RSpec.describe OnePageApplication, type: :controller do
 
         it 'redirects to external domain without double slash' do
           expect(response).to redirect_to('https://example.com/#/index.html')
+        end
+
+        context "with ajax param" do
+          before do
+            get :index, params: { format: :html, ajax: true }
+          end
+
+          it 'does not redirect' do
+            expect(response).to have_http_status(:ok)
+            expect(response.body).to eq('ok')
+          end
         end
       end
 
