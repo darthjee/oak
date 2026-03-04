70c191ab058c464fdc7f73db52c55e8d46920fe3
Fernando Favini <darthjee@gmail.com>
2026-03-03 21:22:22 -0300

    Use Tarquinn dynamic redirec (#61)

--- source/spec/controllers/concerns/one_page_application_spec.rb
+++ source/spec/controllers/concerns/one_page_application_spec.rb
@@ -20,9 +20,9 @@ RSpec.describe OnePageApplication, type: :controller do
   describe '#render_root' do
     subject(:get_request) { get :index, params: parameters }
 
-    context 'without REDIRECT_DOMAIN env variable' do
+    context 'without OAK_REDIRECT_DOMAIN env variable' do
       before do
-        ENV.delete('REDIRECT_DOMAIN')
+        allow(Settings).to receive(:redirect_domain).and_return(nil)
       end
 
       context 'with HTML format' do
@@ -30,7 +30,73 @@ RSpec.describe OnePageApplication, type: :controller do
 
         it 'redirects to hash path' do
           get_request
-          expect(response).to redirect_to('localhost:3000/#/index.html')
+          expect(response).to redirect_to('/#/index.html')
+        end
+      end
+
+      context 'with ajax html request' do
+        let(:parameters) { { format: :html, ajax: true } }
+
+        it 'does not redirect' do
+          get_request
+          expect(response).to have_http_status(:ok)
+        end
+      end
+
+      context 'with json format' do
+        let(:parameters) { { format: :json } }
+
+        it 'does not redirect' do
+          get_request
+          expect(response).to have_http_status(:ok)
+        end
+      end
+    end
+
+    context 'with empty OAK_REDIRECT_DOMAIN env variable' do
+      before do
+        allow(Settings).to receive(:redirect_domain).and_return('')
+      end
+
+      context 'with HTML format' do
+        let(:parameters) { { format: :html } }
+
+        it 'redirects to hash path' do
+          get_request
+          expect(response).to redirect_to('/#/index.html')
+        end
+      end
+
+      context 'with ajax html request' do
+        let(:parameters) { { format: :html, ajax: true } }
+
+        it 'does not redirect' do
+          get_request
+          expect(response).to have_http_status(:ok)
+        end
+      end
+
+      context 'with json format' do
+        let(:parameters) { { format: :json } }
+
+        it 'does not redirect' do
+          get_request
+          expect(response).to have_http_status(:ok)
+        end
+      end
+    end
+
+    context 'with OAK_REDIRECT_DOMAIN env variable' do
+      before do
+        allow(Settings).to receive(:redirect_domain).and_return('example.com')
+      end
+
+      context 'with HTML format' do
+        let(:parameters) { { format: :html } }
+
+        it 'redirects to hash path' do
+          get_request
+          expect(response).to redirect_to('example.com/#/index.html')
         end
       end
 
