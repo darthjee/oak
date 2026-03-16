4f7191d9a1aaa1f5c29cc75ad58af897bdaf7de8
Fernando Favini <darthjee@gmail.com>
2026-03-16 20:14:06 -0300

    Fix Rails/InverseOf

--- source/app/models/oak/category.rb
+++ source/app/models/oak/category.rb
@@ -8,7 +8,7 @@ module Oak
     validates :name, presence: true, length: { maximum: 40 }
 
     has_many :items, class_name: 'Oak::Item', dependent: :destroy
-    has_one :sample_item, -> { order(:id) }, class_name: 'Oak::Item'
+    has_one :sample_item, -> { order(:id) }, class_name: 'Oak::Item', inverse_of: :category
     has_one :main_photo, -> { order(:id) }, class_name: 'Oak::Photo', through: :sample_item
 
     # Association with kinds through category_kinds
--- source/db/migrate/20250512184628_add_file_name_to_photos.rb
+++ source/db/migrate/20250512184628_add_file_name_to_photos.rb
@@ -5,7 +5,7 @@ class AddFileNameToPhotos < ActiveRecord::Migration[7.2]
     # rubocop:disable Rails/NotNullColumn
     add_column :photos, :file_name, :string, null: false
     # rubocop:enable Rails/NotNullColumn
-    
+
     add_index :photos, %i[item_id file_name], unique: true
   end
 end
