84999deddc090c4d821f94a0b979542cfee5b977
Fernando Favini <darthjee@gmail.com>
2026-03-16 20:09:47 -0300

    Fix Rails/NotNullColumn

--- source/db/migrate/20250512184628_add_file_name_to_photos.rb
+++ source/db/migrate/20250512184628_add_file_name_to_photos.rb
@@ -2,8 +2,10 @@
 
 class AddFileNameToPhotos < ActiveRecord::Migration[7.2]
   def change
+    # rubocop:disable Rails/NotNullColumn
     add_column :photos, :file_name, :string, null: false
-
+    # rubocop:enable Rails/NotNullColumn
+    
     add_index :photos, %i[item_id file_name], unique: true
   end
 end
